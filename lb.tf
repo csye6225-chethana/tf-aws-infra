b9e4a778cc018d50fcca49247a317adcd4d2e1a3
Chethana Benny <benny.c@northeastern.edu>
2024-12-02 13:43:07 -0500

    comment unwanted code

--- lb.tf
+++ lb.tf
@@ -64,13 +64,13 @@ resource "aws_lb_target_group" "webapp_tg" {
   }
 }
 
-resource "aws_lb_listener" "webapp_listener" {
-  load_balancer_arn = aws_lb.webapp_lb.arn
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "webapp_listener" {
+#   load_balancer_arn = aws_lb.webapp_lb.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.webapp_tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.webapp_tg.arn
+#   }
+# }
